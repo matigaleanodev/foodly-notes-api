26fc233558b746f0a6a402cef0344ce9a757e19e
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2026-01-27 11:21:16 -0300

    Core: configuración de entorno, validaciones y conexión a MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGO_URI,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

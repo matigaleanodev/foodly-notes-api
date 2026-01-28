104489deaf9e8c1c05d849ea1054709bc5ed6030
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2026-01-28 12:55:34 -0300

    Fix(Swagger): correccion de endpoint en swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RecipesModule } from './recipes/recipes.module';
@@ -22,7 +20,5 @@ import { HttpModule } from '@nestjs/axios';
     }),
     RecipesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
